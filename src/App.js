e6fb5b59b9b7a7f72be9a52a3a3ff19f9626d102
prathamkumar <prathamkumar1985@gmail.com>
2023-09-12 17:18:39 +0530

    Adding Components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Services from "./components/Services";
+import Services from "./components/Services.jsx";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 

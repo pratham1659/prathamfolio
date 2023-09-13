57ab98054204e9653434d76e2ab1ce19e973e253
prathamkumar <prathamkumar1985@gmail.com>
2023-09-13 20:12:53 +0530

    Latest Commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import waveHand from "./img/waving-hand.png";
+import myResume from "../assets/resume-example.pdf";
 
 const skillsImg1 = "https://skillicons.dev/icons?i=html,css";
 const skillsImg2 = "https://skillicons.dev/icons?i=js,react";
 const skillsImg3 = "https://skillicons.dev/icons?i=spring,nodejs";
 
 const Profile = () => {
+  const openPdfInNewTab = () => {
+    window.open(myResume, "_blank");
+  };
   return (
     <div>
       <section id="profile" className="hero">
@@ -66,6 +70,15 @@ const Profile = () => {
                       <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
                     </svg>
                   </a>
+                  {/* button part executive */}
+                  <button class="btn btn-color-2" onClick={openPdfInNewTab}>
+                    Download CV
+                  </button>
+                  <button
+                    class="btn btn-color-1"
+                    onClick={() => (window.location.href = "#contact")}>
+                    Contact Info
+                  </button>
                 </span>
               </div>
               <div className="hero-img"></div>

954e3ebcd93c0ab727101825c0780a8287622781
prathamkumar <prathamkumar1985@gmail.com>
2024-10-25 09:35:00 +0530

    New model file added

--- server/index.js
+++ server/index.js
@@ -11,13 +11,13 @@ app.use(express.json());
 app.post("/commit", async (req, res) => {
   const DATE = moment()
     .subtract(1, "M")
-    .add(10, "d")
+    .subtract(1, "d")
     .add(Math.floor(Math.random() * 24), "hours")
     .add(Math.floor(Math.random() * 60), "minutes")
     .format();
 
   try {
-    await git.add("./*").commit("New model file added", { "--date": DATE }).push("origin", "main");
+    await git.add("./*").commit("handle download error added", { "--date": DATE }).push("origin", "main");
     res.json({ message: "Changes pushed successfully!", date: DATE });
   } catch (err) {
     res.status(500).json({ error: "Failed to push changes", details: err });
